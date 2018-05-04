cfd6f9943939461610b0ebff93e5cb8f71f4f048
Shaun Mulligan <switchflip.shaun@gmail.com>
2018-05-04 17:47:24 +0100

    ...

--- project/main.py
+++ project/main.py
@@ -12,29 +12,25 @@ except IOError:
     file = open("/data/signals.log", 'w')
 
 def signal_term_handler(signal, frame):
-    ts = time.time()
-    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    with open("/data/signals.log","a+") as f:
-        f.write(st + ' got SIGTERM, cleaning up main.py\n')
     print('got SIGTERM, cleaning up main.py\n', flush=True)
-    sys.exit(0)
+    with open("/data/signals.log","a+") as f:
+        f.write('got SIGTERM, cleaning up main.py\n')
+    # sys.exit(0)
 
 def signal_int_handler(signal, frame):
-    ts = time.time()
-    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    with open("/data/signals.log","a+") as f:
-        f.write(st + ' got SIGINT, cleaning up main.py\n')
     print('got SIGINT, cleaning up main.py\n', flush=True)
-    sys.exit(0)
+    with open("/data/signals.log","a+") as f:
+        f.write('got SIGINT, cleaning up main.py\n')
+    # sys.exit(0)
 
 signal.signal(signal.SIGTERM, signal_term_handler)
 signal.signal(signal.SIGINT, signal_int_handler)
 
 print('my pid is ' + str(os.getpid()))
-print("RESIN envvars\n")
-for key in os.environ.keys():
-    if 'RESIN' in key:
-        print(key, os.environ[key])
+# print("RESIN envvars\n")
+# for key in os.environ.keys():
+#     if 'RESIN' in key:
+#         print(key, os.environ[key])
 
 print('Now I\'m just gonna wait around here for something to happen...')
 while True:
