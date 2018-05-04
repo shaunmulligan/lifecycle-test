a9584c53746b8b1e3596eabb9dccfef7c4a3eae0
Shaun Mulligan <switchflip.shaun@gmail.com>
2018-05-04 17:20:17 +0100

    fix python3 print statements

--- project/main.py
+++ project/main.py
@@ -13,25 +13,25 @@ except IOError:
 def signal_int_handler(signal, frame):
     with open("/data/signals.log","a+") as f:
         f.write('got SIGTERM, cleaning up main.py\n')
-    print 'got SIGTERM, cleaning up main.py\n'
+    print('got SIGTERM, cleaning up main.py\n')
     sys.exit(0)
 
 def signal_term_handler(signal, frame):
     with open("/data/signals.log","a+") as f:
         f.write('got SIGINT, cleaning up main.py\n')
-    print 'got SIGINT, cleaning up main.py\n'
+    print('got SIGINT, cleaning up main.py\n')
     sys.exit(0)
 
 signal.signal(signal.SIGTERM, signal_term_handler)
 signal.signal(signal.SIGINT, signal_int_handler)
 
-print 'my pid is ' + str(os.getpid())
-print "RESIN envvars\n"
+print('my pid is ' + str(os.getpid()))
+print("RESIN envvars\n")
 for key in os.environ.keys():
     if 'RESIN' in key:
-        print key, os.environ[key]
+        print(key, os.environ[key])
 
-print 'Now I\'m just gonna wait around here for something to happen...'
+print('Now I\'m just gonna wait around here for something to happen...')
 while True:
     time.sleep(30)
-    print "still waiting around..."
+    print("still waiting around...")
