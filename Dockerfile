a5887cbab0e170818d48a08f0143473f567784e0
Shaun Mulligan <switchflip.shaun@gmail.com>
2018-05-04 17:15:12 +0100

    add sigint handler

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Base our container on a Slim Python + Debian (Jessie) Image
-FROM resin/raspberrypi3-python:2.7
+FROM resin/raspberrypi3-python:3.6-slim
 
 # Set our working directory
 WORKDIR /usr/src/app
--- project/main.py
+++ project/main.py
@@ -10,13 +10,20 @@ try:
 except IOError:
     file = open("/data/signals.log", 'w')
 
-def signal_term_handler(signal, frame):
+def signal_int_handler(signal, frame):
     with open("/data/signals.log","a+") as f:
         f.write('got SIGTERM, cleaning up main.py\n')
     print 'got SIGTERM, cleaning up main.py\n'
     sys.exit(0)
 
+def signal_term_handler(signal, frame):
+    with open("/data/signals.log","a+") as f:
+        f.write('got SIGINT, cleaning up main.py\n')
+    print 'got SIGINT, cleaning up main.py\n'
+    sys.exit(0)
+
 signal.signal(signal.SIGTERM, signal_term_handler)
+signal.signal(signal.SIGINT, signal_int_handler)
 
 print 'my pid is ' + str(os.getpid())
 print "RESIN envvars\n"
@@ -26,5 +33,5 @@ for key in os.environ.keys():
 
 print 'Now I\'m just gonna wait around here for something to happen...'
 while True:
-    time.sleep(10)
+    time.sleep(30)
     print "still waiting around..."
